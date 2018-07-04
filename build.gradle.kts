a334c9ffac728964b4829683e3d1b4aa33ddcb44
Shane Sveller <shanesveller@gmail.com>
2018-07-03 23:02:54 -0500

    Add first Spek test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.plugins.ExtensionAware
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
@@ -20,9 +21,20 @@ group = "com.shanesveller"
 version = "0.1-SNAPSHOT"
 
 apply {
+    // plugin("java")
     plugin("kotlin")
     plugin("groovy")
 }
+
+/*
+test {
+    filters {
+        engines {
+            include("spek")
+        }
+    }
+}
+*/
 
 val kotlin_version: String by extra
 
@@ -34,12 +46,30 @@ repositories {
 
 dependencies {
     compile(kotlinModule("stdlib-jdk8", kotlin_version))
+    compile(kotlinModule("reflect", kotlin_version))
+    compile(kotlinModule("test", kotlin_version))
+    compile(kotlinModule("test-junit", kotlin_version))
+
     compile("org.optaplanner", "optaplanner-core", droolsVersion)
     compile("org.drools", "drools-core", droolsVersion)
     compile("org.drools", "drools-compiler", droolsVersion)
-    testCompile("junit", "junit", "4.12")
     testCompile("org.codehaus.groovy", "groovy", "2.5.0")
     testCompile("org.optaplanner", "optaplanner-test", droolsVersion)
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testRuntimeOnly( "org.junit.jupiter:junit-jupiter-engine:5.2.0" )
+    testRuntime("org.junit.platform:junit-platform-console:1.2.0")
+    testRuntime("org.junit.platform:junit-platform-launcher:1.2.0")
+    // Spek
+    testCompile("org.jetbrains.spek:spek-api:1.1.5") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.1.5") {
+        exclude(group = "org.junit.platform")
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    // Spock
     testCompile("org.spockframework", "spock-core", "1.1-groovy-2.4")
 }
 
