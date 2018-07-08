9083f75610576940cccccb7ccb78f92c49341ff0
Shane Sveller <shanesveller@gmail.com>
2018-07-08 09:39:46 -0500

    Add SPECIAL enum

---
+++ src/main/kotlin/com/shanesveller/falloutBuilder/SpecialAttribute.kt
@@ -0,0 +1,13 @@
+package com.shanesveller.falloutBuilder
+
+// http://fallout.wikia.com/wiki/Fallout_4_primary_statistics
+
+enum class SpecialAttribute {
+    STRENGTH,
+    PERCEPTION,
+    ENDURANCE,
+    CHARISMA,
+    INTELLIGENCE,
+    AGILITY,
+    LUCK
+}
