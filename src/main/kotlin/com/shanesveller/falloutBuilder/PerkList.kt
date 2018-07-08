38988ce675342633b8a62b6cfdc5479da89f5678
Shane Sveller <shanesveller@gmail.com>
2018-07-08 09:38:26 -0500

    Add PerkList class

---
+++ src/main/kotlin/com/shanesveller/falloutBuilder/PerkList.kt
@@ -0,0 +1,9 @@
+package com.shanesveller.falloutBuilder
+
+val PerkList = setOf(
+        Perk("Iron Fist", 1, SpecialAttribute.STRENGTH, 1, 0),
+        Perk("Iron Fist", 2, SpecialAttribute.STRENGTH, 1, 9),
+        Perk("Iron Fist", 3, SpecialAttribute.STRENGTH, 1, 18),
+        Perk("Iron Fist", 4, SpecialAttribute.STRENGTH, 1, 31),
+        Perk("Iron Fist", 5, SpecialAttribute.STRENGTH, 1, 46)
+)
